b298e8acdf84a611f35f2e92c247a5194faf8047
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-10-04 20:44:54 -0300

    docs: production command added in json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import express, { Express } from "express";
+import express, { Express, json } from "express";
 import cors from "cors"
+import router from "./routes/router";
 
 const app:Express = express();
 app.use(cors());
+app.use(json());
+app.use(router);
 
 app.listen(5000, ()=>console.log("servidor funcionando na porta 5000"));
