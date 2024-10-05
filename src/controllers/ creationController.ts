d15b0efba6e921ac2618df871437d70ec45081a2
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-10-05 16:35:10 -0300

    refactor: port configured in env file

--- src/controllers/ creationController.ts
+++ src/controllers/ creationController.ts
@@ -3,13 +3,10 @@ import { Request, Response } from "express";
 export function createCardController(req: Request, res: Response){
     const { headers } = req;
     
-    try{
-        const xApikey =  headers["x-api-key"];   
-        console.log(xApikey)
-        res.status(200).send(req.headers);
-    }
-
-    // console.log(xApikey)
+    const xApikey =  headers["x-api-key"];   
+    console.log(xApikey)
+    res.status(200).send(req.headers);
+    
 
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express, { Express, json } from "express";
 import cors from "cors"
 import router from "./routes/router";
 import errorControle from "./controllers/errorControler";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT || "5000")
 
 const app:Express = express();
 app.use(cors());
 app.use(json());
 app.use(router);
 app.use(errorControle);
-app.listen(5000, ()=>console.log("servidor funcionando na porta 5000"));
+
+app.listen(PORT, ()=>console.log(`server running`));
